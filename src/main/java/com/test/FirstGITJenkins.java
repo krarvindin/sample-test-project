ffc6fc9de05a525448cf5d954b54d69e06f41826
Arvind Krishnan <Akrishnan37@ibahealth.local>
2020-04-10 13:42:46 +0530

    Demo commit for Sample-test-project

---
+++ src/main/java/com/test/FirstGITJenkins.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.test;
+
+/**
+ * @author Akrishnan37
+ *
+ */
+public class FirstGITJenkins {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		System.out.println("First sample project with GIT and Jenkins configure");
+
+	}
+
+}
